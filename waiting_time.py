bbf909674d09284d10b98a977486f188c21baa9f
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-06-19 05:21:03 +0300

    Create waiting_time.py

---
+++ waiting_time.py
@@ -0,0 +1,8 @@
+import keyboard
+import time
+input("Please push the button Enter")
+keyboard.start_recording()
+time.sleep(4)
+events = keyboard.write("Please push the button Enter")
+time.sleep(4)
+events = keyboard.write("Please push the button Enter")
